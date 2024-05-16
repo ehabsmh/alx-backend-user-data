83e5bdba152609a74065a764ed23c0e458fa0b8d
ehabsmh <ehabsmh3@gmail.com>
2024-05-16 21:32:35 +0300

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""5. Encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    byte_str = bytes(password, encoding='utf-8')
+    return bcrypt.hashpw(byte_str, bcrypt.gensalt())
