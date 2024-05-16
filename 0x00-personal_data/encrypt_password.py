dcdc07cccd7b18203795ed6f1a945e006e7d07e2
ehabsmh <ehabsmh3@gmail.com>
2024-05-16 21:33:37 +0300

    pep8 validated

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,5 +4,6 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """Returns a salted, hashed password, which is a byte string."""
     byte_str = bytes(password, encoding='utf-8')
     return bcrypt.hashpw(byte_str, bcrypt.gensalt())
