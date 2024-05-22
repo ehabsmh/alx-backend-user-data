47bb7acb7ccf6f78d754f974af1d8da4133e255a
ehabsmh <ehabsmh3@gmail.com>
2024-05-22 23:57:19 +0300

    1. Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -17,10 +18,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE')
 
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
-else:
-    auth = Auth()
+match auth_type:
+    case "basic_auth":
+        auth = BasicAuth()
+    case "session_auth":
+        auth = SessionAuth()
+    case _:
+        auth = Auth()
+    
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 6. Basic auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Represents Session Authentication-Type
+    """
+    pass
